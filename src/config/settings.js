b93ab8333d77ebc116a3ace3ed59892ce996082a
Garbulinski <ionut-adrian.jurcaneanu@my.fmi.unibuc.ro>
2024-10-31 14:01:29 +0200

    First Commit

---
+++ src/config/settings.js
@@ -0,0 +1,8 @@
+export default {
+	title: `Casa de Vacanta Nufaru Alb`,
+	description: `Un loc linistit si frumos pentru a face vacanta`,
+	url: `https://odyssey-theme.littlesticks.dev`, // No trailing slash!
+	name: `Nufarul Alb`, // The short name of the business or brand name. Used for things like the copyright in the footer.
+  enableThemeSwitcher: true,
+  showLittleSticksPlug: true, // Disable this if you want to remove the Little Sticks plug from the footer. (╯°□°)╯︵ ┻━┻
+};
